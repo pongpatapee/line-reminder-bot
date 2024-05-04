5bf27fbd81a775ccfdb4df03ab19dd2c25a6c67b
pongpatapee <pongpatapee1021@gmail.com>
2024-05-04 12:59:07 -0400

    feat: rewrote in my own way and modularized callbackhander

--- server.go
+++ server.go
@@ -1,17 +1,3 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 package main
 
 import (
@@ -25,85 +11,83 @@ import (
 	"github.com/line/line-bot-sdk-go/v8/linebot/webhook"
 )
 
+type ReminderBot struct {
+	bot           *messaging_api.MessagingApiAPI
+	channelSecret string
+}
+
+func (reminderBot *ReminderBot) callbackHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println("/callback called...")
+
+	callback, err := webhook.ParseRequest(reminderBot.channelSecret, req)
+	if err != nil {
+		log.Printf("Couldn't parse request, error: %+v\n", err)
+		if errors.Is(err, webhook.ErrInvalidSignature) {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	log.Println("Handling events: ")
+
+	for _, event := range callback.Events {
+		log.Printf("/callback called%+v...\n", event)
+
+		// Only hanlding Text message events (early return otherwise)
+		e, ok := event.(webhook.MessageEvent)
+		if !ok {
+			log.Printf("Unsupported message: %T\n", event)
+			return
+		}
+
+		message, ok := e.Message.(webhook.TextMessageContent)
+		if !ok {
+			log.Printf("Unsupported message content: %T\n", e.Message)
+			return
+		}
+
+		if _, err = reminderBot.bot.ReplyMessage(
+			&messaging_api.ReplyMessageRequest{
+				ReplyToken: e.ReplyToken,
+				Messages: []messaging_api.MessageInterface{
+					messaging_api.TextMessage{
+						Text: message.Text,
+					},
+				},
+			},
+		); err != nil {
+			log.Print(err)
+		} else {
+			log.Println("Sent text reply.")
+		}
+
+	}
+}
+
 func main() {
 	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
-	bot, err := messaging_api.NewMessagingApiAPI(
-		os.Getenv("LINE_CHANNEL_TOKEN"),
-	)
+	bot, err := messaging_api.NewMessagingApiAPI(os.Getenv("LINE_CHANNEL_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		log.Println("/callback called...")
+	reminderBot := ReminderBot{
+		channelSecret: channelSecret,
+		bot:           bot,
+	}
 
-		cb, err := webhook.ParseRequest(channelSecret, req)
-		if err != nil {
-			log.Printf("Cannot parse request: %+v\n", err)
-			if errors.Is(err, webhook.ErrInvalidSignature) {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-
-		log.Println("Handling events...")
-		for _, event := range cb.Events {
-			log.Printf("/callback called%+v...\n", event)
-
-			switch e := event.(type) {
-			case webhook.MessageEvent:
-				switch message := e.Message.(type) {
-				case webhook.TextMessageContent:
-					if _, err = bot.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
-							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: message.Text,
-								},
-							},
-						},
-					); err != nil {
-						log.Print(err)
-					} else {
-						log.Println("Sent text reply.")
-					}
-				case webhook.StickerMessageContent:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerId, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
-							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: replyMessage,
-								},
-							},
-						}); err != nil {
-						log.Print(err)
-					} else {
-						log.Println("Sent sticker reply.")
-					}
-				default:
-					log.Printf("Unsupported message content: %T\n", e.Message)
-				}
-			default:
-				log.Printf("Unsupported message: %T\n", event)
-			}
-		}
+	http.HandleFunc("/ping", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, "Wh1fty line reminder bot")
 	})
 
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	http.HandleFunc("/callback", reminderBot.callbackHandler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		port = "8080"
 	}
-	fmt.Println("http://localhost:" + port + "/")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Server listening on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
